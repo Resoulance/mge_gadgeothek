e2d3669a120340e2c4555f6baeb36489e743cfe7
Conan <conan.le@gmail.com>
2017-10-10 21:02:19 +0200

    Alternative Layout for Loginscreen, few code changes

--- Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/MainActivity.java
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/MainActivity.java
@@ -1,15 +1,11 @@
 package net.resoulance.gadgeothek;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import net.resoulance.gadgeothek.service.Callback;
-import net.resoulance.gadgeothek.service.LibraryService;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Button toLogin = (Button) findViewById(R.id.toLoginButton);
         Button toRegister = (Button) findViewById(R.id.toRegisterButton);
         Button toRegisterAlternative = (Button) findViewById(R.id.toRegsterAlternative);
+        Button toLoginAlternative = (Button) findViewById(R.id.toLoginAlternative);
 
 
         toLogin.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, RegisterAlternativeActivity.class);
                 startActivity(intent);
                 /*setContentView(R.layout.activity_register_alternative);*/
+            }
+        });
+
+        toLoginAlternative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                setContentView(R.layout.activity_login_alternative);
+           
             }
         });
 
