b57d2378991d7750c9528d037aad56eaf951d0d8
Conan <conan.le@gmail.com>
2017-10-10 17:07:02 +0200

    added hsr logo

--- Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/LoginActivity.java
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/LoginActivity.java
@@ -12,7 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__login);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
