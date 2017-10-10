2ce398422ead6f3b010fa3ad495194cdd9b03b6b
Conan <conan.le@gmail.com>
2017-10-10 18:38:14 +0200

    mainActivity as entry point for other activity

---
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/MainActivity.java
@@ -0,0 +1,34 @@
+package net.resoulance.gadgeothek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import net.resoulance.gadgeothek.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button toLogin = (Button) findViewById(R.id.toLoginButton);
+        Button toRegister = (Button) findViewById(R.id.toRegisterButton);
+
+        toLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                setContentView(R.layout.activity_login);
+            }
+        });
+
+        toRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                setContentView(R.layout.activity_register);
+            }
+        });
+    }
+}
