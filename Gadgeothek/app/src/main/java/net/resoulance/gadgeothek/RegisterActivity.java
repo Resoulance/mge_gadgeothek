de625de4ed5ff571e0445de4772f3510d491ab6e
Conan <conan.le@gmail.com>
2017-10-10 16:57:37 +0200

    clean up

--- Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/RegisterActivity.java
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/RegisterActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Activity_Login extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__login);
+        setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
