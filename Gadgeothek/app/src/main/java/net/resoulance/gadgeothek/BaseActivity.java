5e9f3fadb96dcfc7527b794ebb538bbebc2cb9ff
Sascha Gschwind <sascha.gschwind.88@gmail.com>
2017-10-16 14:16:52 +0200

    Add toast to test settings selected

--- Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/BaseActivity.java
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/BaseActivity.java
@@ -1,10 +1,12 @@
 package net.resoulance.gadgeothek;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by sasch on 16.10.2017.
@@ -23,9 +25,21 @@ abstract class BaseActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.option_settings:
                 // TODO: Code hier
+                Context context1 = getApplicationContext();
+                CharSequence text1 = "Settings clicked!";
+                int duration1 = Toast.LENGTH_SHORT;
+
+                Toast toast1 = Toast.makeText(context1, text1, duration1);
+                toast1.show();
                 return true;
             case R.id.option_logout:
                 // TODO Code hier
+                Context context2 = getApplicationContext();
+                CharSequence text2 = "Logout clicked!";
+                int duration2 = Toast.LENGTH_SHORT;
+
+                Toast toast2 = Toast.makeText(context2, text2, duration2);
+                toast2.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
