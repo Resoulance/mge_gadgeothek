66c442fe6638cd96e990cc1f8713e36b763895ac
Conan Le <cle@hsr.ch>
2017-10-17 16:33:56 +0200

    Check for Logout Button
    Checks if logged in for logout

--- Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/BaseActivity.java
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/BaseActivity.java
@@ -2,6 +2,7 @@ package net.resoulance.gadgeothek;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -37,20 +38,39 @@ abstract class BaseActivity extends AppCompatActivity {
                     @Override
                     public void onCompletion(Boolean input) {
 
-                        Toast toast = Toast.makeText(getApplicationContext(), "Erfolgreich ausgeloggt", Toast.LENGTH_SHORT);
-                        toast.show();
-                        Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
+                        if (LibraryService.isLoggedIn()) {
+
+                            Toast toast = Toast.makeText(getApplicationContext(), "Erfolgreich ausgeloggt", Toast.LENGTH_SHORT);
+                            toast.show();
+                            Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Bereits ausgeloggt", Toast.LENGTH_SHORT);
+                            toast.show();
+
+                        }
                     }
 
                     @Override
                     public void onError(String message) {
-                        Toast toast = Toast.makeText(getApplicationContext(), "Logout Fehler", Toast.LENGTH_SHORT);
+                                                Toast toast = Toast.makeText(getApplicationContext(), "Logout Fehler", Toast.LENGTH_SHORT);
                         toast.show();
 
                     }
                 });
+
+                return true;
+            case R.id.option_Tester:
+
+                SharedPreferences prefs = this.getSharedPreferences("net.resoulance.app", Context.MODE_PRIVATE);
+
+
+                //Werte aus der Settings lesen und dann über den Toaster zeigen
+
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Tester", Toast.LENGTH_SHORT);
+                toast.show();
 
                 return true;
             default:
@@ -61,7 +81,7 @@ abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         setTitle(null);
 
