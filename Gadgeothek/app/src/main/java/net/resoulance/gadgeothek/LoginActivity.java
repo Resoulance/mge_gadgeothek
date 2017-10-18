ace51176e39e6dea4a1c6108ed08a735df8e1067
Conan Le <cle@hsr.ch>
2017-10-18 09:00:22 +0200

    new Activity
    Splashscreen and Logic for automatic login

--- Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/LoginActivity.java
+++ Gadgeothek/app/src/main/java/net/resoulance/gadgeothek/LoginActivity.java
@@ -23,6 +23,7 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         TextView toRegister = (TextView) findViewById(R.id.toregisterTextView);
